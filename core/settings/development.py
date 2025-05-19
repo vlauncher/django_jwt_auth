fe3cdeec36d5113368d999750d45319e465f7581
vlauncher <v2dent@gmail.com>
2025-05-19 12:03:05 +0100

    project setup

---
+++ core/settings/development.py
@@ -0,0 +1,12 @@
+from core.settings.base import *
+
+DEBUG = environ.get('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
